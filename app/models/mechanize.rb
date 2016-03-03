20edb6813efc57d4da93814281ddc9514577ecc7
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-02 23:21:00 -0600

    Scrape title, author (with 'by'...), and price. Issue with scraping photo_url link. Currently stored as distinct arrays.

--- app/models/mechanize.rb
+++ app/models/mechanize.rb
@@ -7,6 +7,26 @@ agent = Mechanize.new
 
 page = agent.get('http://www.amazon.com/gp/bestsellers/2015/books/')
 
-page.links.each do |link|
-  puts link.text
+titles = []
+page.search('.zg_title').each do |title|
+  titles << title.text.strip
 end
+puts titles
+
+authors = []
+page.search('.zg_byline').each do |author|
+  authors << author.text.strip
+end
+puts authors
+
+prices = []
+page.search('.zg_price .price').each do |price|
+  prices << price.text.strip
+end
+puts prices
+
+# photo_urls = []
+# page.search('.zg_itemImageImmersion img').each do |photo_url|
+#   photo_urls << photo_url.text.strip
+# end
+# puts photo_urls
