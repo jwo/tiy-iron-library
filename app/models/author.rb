673d11864d63d98b247f8b7942cb88d5875b2847
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-02 22:24:15 -0600

    Add validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ActiveRecord::Base
   has_many :books
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :bio, presence: true
 
   def full_name
     [first_name, last_name].join(" ")
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ActiveRecord::Base
   belongs_to :author
 
   validates :title, :price, :author_id, presence: true
+  validates_numericality_of :price, :greater_than => 0, :less_than => 1000
+  validates_format_of :price, :with => /\A\d+(?:\.\d{0,2})?\z/
+
 end
