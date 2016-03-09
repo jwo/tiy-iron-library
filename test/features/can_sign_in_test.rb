27a311e886abb79af1499a0bc6021c11547218c4
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-09 13:39:20 -0600

    Add sign-in test

---
+++ test/features/can_sign_in_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class CanSignInTest < Capybara::Rails::TestCase
+  setup do
+    User.create! name: "Bob", email: "bob@example.com", password: "12345678"
+  end
+  test "Can sign in" do
+    visit root_path
+    click_link "All Recipes"
+    fill_in "Email", with: "bob@example.com"
+    fill_in "Password", with: "12345678"
+    click_button "Sign In"
+
+    assert_content page, "Bob"
+  end
+end
