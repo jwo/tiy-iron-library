99abf2bbc67456518073372b650f031aa05b4b1a
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-09 19:11:32 -0600

    Adjust test suite for 0 failures.

--- test/features/can_sign_in_test.rb
+++ test/features/can_sign_in_test.rb
@@ -3,10 +3,12 @@ require "test_helper"
 class CanSignInTest < Capybara::Rails::TestCase
   setup do
     User.create! name: "Bob", email: "bob@example.com", password: "12345678"
+    Book.create! title:"Hamlet", photo_url:"http://t0.gstatic.com/images?q=tbn:ANd9GcSJC7FDq-z6NlSNw-45Ra_v3wDYqU3BPHdxEQyTVPzC99vj9D6R", price:"5.00", author_id: 1
+    Author.create! first_name: "William", last_name: "Shakespeare", bio: "William Shakespeare was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's pre-eminent dramatist. He is often called England's national poet, and the 'Bard of Avon'. ", id: 1
   end
   test "Can sign in" do
     visit root_path
-    click_link "Books"
+    click_link "Hamlet"
     fill_in "Email", with: "bob@example.com"
     fill_in "Password", with: "12345678"
     click_button "Sign In"
