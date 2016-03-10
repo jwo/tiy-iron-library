479efdd871b88a0ff506cf93e204eabd748a4073
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-09 23:07:47 -0600

    Add methods to store location and redirect in application controller. Call methods in user, session, book, and author controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,6 +2,7 @@ class AuthorsController < ApplicationController
   before_action do
     @current_user = User.find_by id: session[:user_id]
     if @current_user.blank?
+      store_location
       redirect_to sign_in_path
     end
   end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,7 +11,7 @@ class UserController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_back_or_default root_path
     else
       render :new
     end
