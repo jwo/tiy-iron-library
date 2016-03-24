28e8ab60025cc681f5041256a173dc5c84ae4811
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-23 19:49:44 -0500

    Add books controller and routes

---
+++ app/controllers/api/books_controller.rb
@@ -0,0 +1,57 @@
+class Api::BooksController < ApplicationController
+  def index
+    @books = Book.all.order("title asc")
+  end
+
+  def show
+    @book = Book.find_by(id: params[:id])
+  end
+
+  def create
+    @book = Book.new book_params
+
+    url = params[:book][:photo_url]
+    if url.present?
+      open(url, "rb") do |file|
+        @book.photo = file
+      end
+    end
+
+    if @book.save
+      render :index
+    else
+      render json: {errors: @book.errors}, status: 422
+    end
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+
+    url = params[:book][:photo_url]
+    if url.present?
+      open(url, "rb") do |file|
+        @book.photo = file
+      end
+    end
+
+    if @book.update book_params
+      render :show
+    else
+      render json: {errors: @book.errors}, status: 422
+    end
+  end
+
+  def destroy
+    @book = Book.find_by(id: params[:id])
+    @book.destroy
+    head :ok
+  end
+
+  def authors
+    @authors = Author.all.order("last_name asc")
+  end
+
+  def book_params
+    params.require(:books).permit(:title, :photo, :price, :author_id)
+  end
+end
