4d921e7d53dd910a3d21446341d577a42441dc3b
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-24 13:38:40 -0500

    Change 'protect_from_forgery' so that api can be used without oauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   def store_location
     session[:return_to] = request.fullpath if request.get? && controller_name != "user" && controller_name != "session"
   end
